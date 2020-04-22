df50ab328ac4c2ed131a40331a9852d560cf35d0
zerotosilver <zerosilvertwo@gmail.com>
2020-04-22 14:58:06 +0900

    first management commit

---
+++ src/main/java/meetingroom/Reserved.java
@@ -0,0 +1,31 @@
+
+package meetingroom;
+
+public class Reserved extends AbstractEvent {
+
+    private Long id;
+    private String reserveId;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getReserveId() {
+        return reserveId;
+    }
+
+    public void setReserveId(String reserveId) {
+        this.reserveId = reserveId;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
